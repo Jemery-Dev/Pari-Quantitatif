623892c0d7b3f8cdffd363a26cb9bdca9fc3b942
mvalour <valour.math@gmail.com>
2024-03-19 00:11:48 +0100

    redirection des pages fonctionnelle (avec un layout)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { AboutComponent } from './about/about.component';
+import { LayoutComponent } from './layout/layout.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavbarComponent,
+    AboutComponent,
+    LayoutComponent
   ],
   imports: [
     BrowserModule,
