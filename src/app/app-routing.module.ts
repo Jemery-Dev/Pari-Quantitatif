af5d6794ce3a8812bab8fcc80b7a40a4052dc963
mvalour <mathis.valour@etu.univ-lyon1.fr>
2024-03-23 20:07:25 +0100

    ff

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { HomeComponent } from './views/home/home.component';
-import { AboutComponent } from './views/about/about.component';
+import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
-  { path: '', component: HomeComponent },
-  { path: 'about', component: AboutComponent }
+  { path: '', component: HomeComponent }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,20 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './views/home/home.component';
-import { NavbarComponent } from './layout/navbar/navbar.component';
-import { AboutComponent } from './views/about/about.component';
-import { LayoutComponent } from './layout/layout.component';
-import { HeaderComponent } from './layout/header/header.component';
+import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './header/header.component';
 import { StartButtonComponent } from './utils/start-button/start-button.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    NavbarComponent,
-    AboutComponent,
-    LayoutComponent,
     HeaderComponent,
     StartButtonComponent
   ],
