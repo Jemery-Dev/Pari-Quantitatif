ef3c20d1ce5aaf822491cd2cc39141f106eb726a
Jérémy Girard <75862623+Jemery-Dev@users.noreply.github.com>
2024-04-17 15:42:00 +0200

    Ajout du chat (non fonctionnel à cause du serveur)

--- src/app/chat/chat.component.ts
+++ src/app/chat/chat.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { WebSocketService } from './web-socket.service';
 
 @Component({
   selector: 'app-chat',
   templateUrl: './chat.component.html',
-  styleUrl: './chat.component.scss'
+  styleUrls: ['./chat.component.scss']
 })
-export class ChatComponent {
+export class ChatComponent implements OnInit, OnDestroy {
+  constructor(public webSocketService: WebSocketService) { }
 
+  ngOnInit(): void {
+    this.webSocketService.openWebSocketConnection();
+  }
+
+  ngOnDestroy(): void {
+    this.webSocketService.closeWebSocketConnection();
+  }
+
+  sendMessage(user: string, message: string): void {
+    this.webSocketService.sendWebSocketMessage({ user, message });
+  }
+}
+
+export class WebSocketChat {
+  user: string;
+  message: string;
+
+  constructor(user: string, message: string) {
+    this.user = user;
+    this.message = message;
+  }
 }
