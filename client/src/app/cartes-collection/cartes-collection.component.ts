a93c3aa2379437dda8f0315225f04e84517b6f1d
Jérémy Girard <75862623+Jemery-Dev@users.noreply.github.com>
2024-07-04 14:48:31 +0200

    Première version

--- client/src/app/cartes-collection/cartes-collection.component.ts
+++ client/src/app/cartes-collection/cartes-collection.component.ts
@@ -14,20 +14,17 @@ interface CardData {
   }[];
 }
 
-function flipCard(event: { currentTarget: any; }) {
-  const card = event.currentTarget;
-  card.classList.toggle('flipped');
-}
-
 @Component({
   selector: 'app-cartes-collection',
   templateUrl: './cartes-collection.component.html',
-  styleUrl: './cartes-collection.component.scss'
+  styleUrls: ['./cartes-collection.component.scss']
 })
 export class CartesCollectionComponent implements OnInit {
 
   cards: CardData[] = [];
   categorieSelectionne: string = "";
+  flippedCards: { [key: string]: boolean } = {};
+
   constructor(private cardService: CardService) { }
 
   ngOnInit(): void {
@@ -41,11 +38,21 @@ export class CartesCollectionComponent implements OnInit {
     );
   }
 
-  flipCard(id: string) {
-    const card = document.getElementById('card-' + id);
-    if (card) {
-      card.classList.toggle('flipped');
-    }
+  flipCard(category: string, theme: string) {
+    const cardKey = `${category}-${theme}`;
+    this.flippedCards[cardKey] = !this.flippedCards[cardKey];
   }
 
+  isFlipped(category: string, theme: string): boolean {
+    const cardKey = `${category}-${theme}`;
+    return !!this.flippedCards[cardKey];
+  }
+
+  filteredCards() {
+    if (!this.categorieSelectionne) {
+      return this.cards.flatMap(card => card.cartes.map(carte => ({ ...carte, nom: card.nom })));
+    }
+    const selectedCard = this.cards.find(card => card.nom === this.categorieSelectionne);
+    return selectedCard ? selectedCard.cartes.map(carte => ({ ...carte, nom: selectedCard.nom })) : [];
+  }
 }
